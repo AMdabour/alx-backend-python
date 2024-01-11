f92524e5395e5e6423219ac16a29de8fbf9d6aeb
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-11 16:12:00 +0000

    initial commit add func type annotated

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a type annotated function for adding two numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """adds two nums and return their sum"""
+    return a + b
