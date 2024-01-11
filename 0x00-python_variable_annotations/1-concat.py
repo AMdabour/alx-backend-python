43cbe2dc2df12c6e4f69fabf769900859c8bd1a5
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-11 16:18:47 +0000

    concat ...

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a type annotated func for concatenating two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concates str1 and str2, returning the result"""
+    return str1 + str2
