7c48e94391ded8eb62a90989e8d79b1c0632e8f8
a-dabour <ah910905@gmail.com>
2024-02-05 12:50:23 +0200

    mock a method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """the unit test for utils.access_nested_map"""
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 import unittest
 # import requests
@@ -45,3 +45,26 @@ class TestGetJson(unittest.TestCase):
         result = get_json(url)
         mock_get.assert_called_once_with(url)
         self.assertEqual(result, expected)
+
+
+class TestMemoize(unittest.TestCase):
+    """test memoize"""
+    def test_memoize(self):
+        """testing method"""
+        class TestClass:
+            """TestClass"""
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value=lambda: 42
+        ) as m:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            m.assert_called_once
